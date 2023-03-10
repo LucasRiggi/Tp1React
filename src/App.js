0f3082e68d94f76c9e79d12ed808363c273156b6
Diego Ranon <2040616@cmontmorency.qc.ca>
2023-03-09 22:33:49 -0500

    Premier commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from '../assets/Logomomo.png';
-import '../style/App.css'
+import '../style/App.css';
+import MainNavigation from "./shared/Navigation/MainNavigation";
 
 function App() {
   return (
+  <Router>
+    <MainNavigation />
+    <main>
+    
     <div className="App">
       <header className="App-header">
       <img src={logo} alt='logo montmorency' className='lmj-logo' />
@@ -10,6 +15,8 @@ function App() {
       <p>wesh salut a tous c metre gibs donner moi 200 euros pour que je puisse finir mon album en france</p>
       </header>
     </div>
+    </main>  
+  </Router>
   );
 }
 
